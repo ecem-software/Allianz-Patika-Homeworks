a5bc30ade130839845b34d8b44e31d3c4f0df316
Ecem Terzi <ecemterz@gmail.com>
2023-08-04 14:05:11 +0300

    commit

---
+++ Üslü Sayı Hesaplayan Program/BodyIndexCalculation/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double height, weight,BodyWeightIndex;
+      Scanner scanner=new Scanner(System.in);
+        System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz :");
+        height=scanner.nextDouble();
+        System.out.println("Lütfen kilonuzu giriniz :");
+        weight=scanner.nextDouble();
+        BodyWeightIndex=weight/(height*height);
+        System.out.println("Vücut Kitle İndeksiniz : " + BodyWeightIndex);
+
+
+
+
+    }
+}
